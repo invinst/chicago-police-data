b62385ed57f9dda5e87a8cae6ece5d7ea2f560c1
Roman Rivera <roman@invisibleinstitute.com>
2017-07-22 17:43:19 -0500

    more symbolic link updates

--- individual/p050032_all-members-rank/all-members/clean/src/clean.py
+++ individual/p050032_all-members-rank/all-members/clean/src/clean.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 from CleaningFunctions import *
 import sys
-infile = sys.argv[1]
-outfile = sys.argv[-1]
+infile = "../input/all-members.csv"
+outfile = "../output/all-members.csv"
 df = pd.read_csv(infile)
 df = CleanData(df)
 df.to_csv(outfile, index=False)
