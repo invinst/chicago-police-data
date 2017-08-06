4ee6029887925dba49b5187e5eb95e0862d5e0f2
Roman Rivera <roman@invisibleinstitute.com>
2017-08-06 12:10:42 -0500

    proper styling for import and clean steps, redoing unit history cleaning

--- individual/16-1105_all-sworm-employees/clean/src/clean.py
+++ individual/16-1105_all-sworm-employees/clean/src/clean.py
@@ -1,10 +1,35 @@
 import pandas as pd
+import __main__
 
-from cleaning_functions import clean_data, input_opts, output_opts
+from cleaning_functions import clean_data
+import setup
 
-input_file = 'input/unit-history.csv.gz'
-output_file = 'output/unit-history.csv.gz'
 
-df = pd.read_csv(input_file, **input_opts)
+def get_setup():
+    ''' encapsulates args.
+        calls setup.do_setup() which returns constants and logger
+        constants contains args and a few often-useful bits in it
+        including constants.write_yamlvar()
+        logger is used to write logging messages
+    '''
+    script_path = __main__.__file__
+    args = {
+        'input_file': 'input/unit-history.csv.gz',
+        'output_file': 'output/unit-history.csv.gz'
+        }
+
+    assert (args['input_file'].startswith('input/') and
+            args['input_file'].endswith('.csv.gz')),\
+        "input_file is malformed: {}".format(args['input_file'])
+    assert (args['output_file'].startswith('output/') and
+            args['output_file'].endswith('.csv.gz')),\
+        "output_file is malformed: {}".format(args['output_file'])
+
+    return setup.do_setup(script_path, args)
+
+
+cons, log = get_setup()
+
+df = pd.read_csv(cons.input_file)
 df = clean_data(df)
-df.to_csv(output_file, **output_opts)
+df.to_csv(cons.output_file, **cons.csv_opts)
--- individual/share/src/cleaning_functions.py
+++ individual/share/src/cleaning_functions.py
@@ -1,13 +1,18 @@
-import pandas as pd
 import re
 import numpy as np
+import pandas as pd
 
-input_opts = {'compression' : 'gzip'}
-output_opts = {'index' : False, 'compression' : 'gzip'}
+gender_df = pd.read_csv('hand/gender_dictionary.csv')
+gender_dict = dict(zip(gender_df.Original, gender_df.Standard))
+
+race_df = pd.read_csv('hand/race_dictionary.csv')
+race_dict = dict(zip(race_df.Original, race_df.Standard))
+
 
 def list_diff(l1, l2): return list(set(l1) - set(l2))
 
-def clean_int(x, na_value = -999):
+
+def clean_int(x, na_value=-999):
     if isinstance(x, str):
         if re.match('[a-zA-Z]', x):
             return na_value
@@ -18,19 +23,22 @@ def clean_int(x, na_value = -999):
     else:
         return na_value
 
-def clean_gender(x, gender_dict = {'M' : 'MALE', 'F' : 'FEMALE', 'X' : 'UNKNOWN'}):
+
+def clean_gender(x):
     x = x.upper()
     if x in gender_dict.values():
         return x
     else:
         return gender_dict[x]
 
-def clean_race(x, race_dict = {'WHI':'WHITE', 'BLK':'BLACK', 'S':'WHITE', 'API':'API', 'I':'OTHER', 'U':'UNKNOWN', 'WWH':'HISPANIC', 'ASIAN/PACIFIC ISLANDER' : 'API', 'BLACK HISPANIC': 'BLACK', 'WHITE HISPANIC' : 'HISPANIC', 'AMER IND/ALASKAN NATIVE' : 'OTHER'}):
+
+def clean_race(x):
     x = x.upper()
     if x in race_dict.values():
         return x
     else:
         return race_dict[x]
+
 
 def clean_dates(df):
     df_cols = df.columns.values
@@ -38,29 +46,33 @@ def clean_dates(df):
     for col in df_cols:
         col_suffix = col.split('.')[:-1]
         dt_df['.'.join(col_suffix + ["Date"])] = \
-        pd.to_datetime(df[col]).dt.date
+            pd.to_datetime(df[col]).dt.date
         if 'time' in col:
             dt_df['.'.join(col_suffix + ["Time"])] = \
-            pd.to_datetime(df[col]).dt.time
+                pd.to_datetime(df[col]).dt.time
     return dt_df
+
 
 def extract_suffix_names(x):
     suffixes = ('II', 'III', 'IV', 'JR', 'SR')
     suffix = [w for w in x.split(" ") if w in suffixes]
     return suffix[0] if suffix else ""
 
+
 def extract_middle_initial(x):
     xs = x.split(' ')
     if (len(xs) > 1 and
-        len(xs[0]) == 1 and
-        len(xs[1]) > 1):
+            len(xs[0]) == 1 and
+            len(xs[1]) > 1):
         return xs[0]
     else:
         return ""
 
+
 def strip_names(x):
     x = re.sub(r'[^\w\s]', '', x)
     return ' '.join(x.split())
+
 
 def clean_last_names(x):
     x = strip_names(x)
@@ -68,41 +80,44 @@ def clean_last_names(x):
     x = x.replace(suffix, "")
     return [''.join(x.split()), suffix]
 
+
 def clean_first_names(x):
     x = strip_names(x)
     mi = extract_middle_initial(x)
     return [''.join(x.replace(mi + " ", "").split()), mi]
+
 
 def split_full_names(names, ln='Last.Name', fn='First.Name'):
     names.fillna(",", inplace=True)
     names = names.map(lambda x: x if re.search('[a-zA-Z]', x) else ",")
     names = names.map(lambda x: x.rsplit(',', 1))
     return pd.DataFrame(names.values.tolist(),
-                            columns = [ln, fn])
+                        columns=[ln, fn])
 
 
 def split_names(names, main_name, sub_name, clean_func):
     names = names.map(str.upper)
     names = names.map(clean_func)
     return pd.DataFrame(names.values.tolist(),
-                        columns = [main_name, sub_name])
+                        columns=[main_name, sub_name])
 
 
 def clean_names(df):
     df_cols = df.columns.values
-    if 'Full.Name' in df_cols and 'Last.Name' not in df_cols: 
+    if ('Full.Name' in df_cols and
+            'Last.Name' not in df_cols):
         name_df = split_full_names(df['Full.Name'])
         last = split_names(name_df['Last.Name'],
-                        'Last.Name', 'Suffix.Name',
-                        clean_last_names)
-        first = split_names(name_df['First.Name'], 
-                        'First.Name', 'Middle.Initial', 
-                        clean_first_names)
+                           'Last.Name', 'Suffix.Name',
+                           clean_last_names)
+        first = split_names(name_df['First.Name'],
+                            'First.Name', 'Middle.Initial',
+                            clean_first_names)
     else:
         df = df.fillna("")
         last = split_names(df['Last.Name'],
-                        'Last.Name', 'Suffix.Name',
-                        clean_last_names)
+                           'Last.Name', 'Suffix.Name',
+                           clean_last_names)
         first = split_names(df['First.Name'],
                             'First.Name', 'Middle.Initial',
                             clean_first_names)
@@ -111,24 +126,25 @@ def clean_names(df):
 
     if 'Middle.Initial' in df_cols:
         print('Middle Initial Conflicts:',
-                name_df.ix[(df['Middle.Initial'] != '') & 
-                (name_df['Middle.Initial'] != '')])
-        name_df.ix[df['Middle.Initial'] != '','Middle.Initial'] = \
-        df.ix[df['Middle.Initial'] != '', 'Middle.Initial']
+              name_df.ix[(df['Middle.Initial'] != '') &
+                         (name_df['Middle.Initial'] != '')])
+        name_df.ix[df['Middle.Initial'] != '', 'Middle.Initial'] = \
+            df.ix[df['Middle.Initial'] != '', 'Middle.Initial']
 
     if 'Suffix.Name' in df_cols:
         print('Suffix Name Conflicts:',
-                name_df.ix[(df['Suffix.Name'] != '') & 
-                (name_df['Suffix.Name'] != '')])
-        name_df.ix[df['Suffix.Name'] != '','Suffix.Name'] = \
-        df.ix[df['Suffix.Name'] != '', 'Suffix.Name']
+              name_df.ix[(df['Suffix.Name'] != '') &
+                         (name_df['Suffix.Name'] != '')])
+        name_df.ix[df['Suffix.Name'] != '', 'Suffix.Name'] = \
+            df.ix[df['Suffix.Name'] != '', 'Suffix.Name']
 
     return name_df
 
-def clean_data(df, skip_cols = []):
+
+def clean_data(df, skip_cols=[]):
     col_df = pd.read_csv('hand/column_dictionary.csv')
-    name_cols = col_df.loc[col_df["Type"]=='Name', 'Column'].tolist()
-    int_cols = col_df.loc[col_df["Type"]=='Int', 'Column'].tolist()
+    name_cols = col_df.loc[col_df["Type"] == 'Name', 'Standard'].tolist()
+    int_cols = col_df.loc[col_df["Type"] == 'Int', 'Standard'].tolist()
     df_cols = df.columns.values
 
     if 'Gender' in df_cols and 'Gender' not in skip_cols:
@@ -142,11 +158,13 @@ def clean_data(df, skip_cols = []):
         df[col] = df[col].map(clean_int)
 
     if [col for col in df_cols if 'Date' in col]:
-        dt_df = df[[dc for dc in df_cols if 'Date' in  dc]]
-        df = df[list_diff(df.columns, dt_df.columns)].join(clean_dates(dt_df))
+        dt_df = df[[dc for dc in df_cols if 'Date' in dc]]
+        df = df[list_diff(df.columns,
+                dt_df.columns)].join(clean_dates(dt_df))
 
     if [col for col in df_cols if col in name_cols]:
         name_df = df[[col for col in df_cols if col in name_cols]]
-        df = df[list_diff(df.columns, name_df.columns)].join(clean_names(name_df))
+        df = df[list_diff(df.columns,
+                name_df.columns)].join(clean_names(name_df))
 
     return df
