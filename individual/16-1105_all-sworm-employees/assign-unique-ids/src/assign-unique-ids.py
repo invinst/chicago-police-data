8fd0c6a8b6851619c6f0a9f51289eabb5705788e
Roman Rivera <roman@invisibleinstitute.com>
2017-08-06 17:41:58 -0500

    assigning unique ids to unit history and inspecting files

--- individual/16-1105_all-sworm-employees/assign-unique-ids/src/assign-unique-ids.py
+++ individual/16-1105_all-sworm-employees/assign-unique-ids/src/assign-unique-ids.py
@@ -9,7 +9,7 @@ output_demo_file = 'output/unit-history_demographics.csv.gz'
 
 df = pd.read_csv(input_file, **input_opts)
 
-stars = ["Star" + str(i) for i in range(1,11)]
+stars = ["Star" + str(i) for i in range(1, 11)]
 id_cols = [
     "First.Name", "Last.Name", "Middle.Initial", "Suffix.Name",
     "Appointed.Date", "Current.Age", "Gender", "Race"
@@ -19,6 +19,6 @@ df = assign_unique_ids(df, 'TID', id_cols)
 df.to_csv(output_file, **output_opts)
 
 agg_df = aggregate_data(df, 'TID', id_cols,
-                max_cols=stars,
-                current_cols = ['Unit'], time_col = 'Effective.Date')
+                        max_cols=stars,
+                        current_cols=['Unit'], time_col='Effective.Date')
 agg_df.to_csv(output_demo_file, **output_opts)
--- individual/share/src/assign_unique_ids_functions.py
+++ individual/share/src/assign_unique_ids_functions.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
-input_opts = {'compression': 'gzip'}
-output_opts = {'index': False, 'compression': 'gzip'}
 
 def remove_duplicates(df, cols=[]):
-    if not cols: cols = df.columns.tolist()
+    if not cols:
+        cols = df.columns.tolist()
     return df[~df.duplicated(subset=cols, keep=False)]
+
 
 def keep_duplicates(df, cols):
     return df[df.duplicated(subset=cols, keep=False)].sort_values(cols)
+
 
 def assign_unique_ids(df, uid, id_cols):
     dfu = df[id_cols].drop_duplicates()
@@ -16,14 +17,16 @@ def assign_unique_ids(df, uid, id_cols):
     dfu[uid] = dfu.index + 1
     return df.merge(dfu, on=id_cols, how='left')
 
+
 def max_aggregate(df, id_cols, max_cols):
     df = df.drop_duplicates()
     df = df.groupby(id_cols, as_index=False)[max_cols]
     return df.agg(max)
 
+
 def order_aggregate(df, id_cols,
                     agg_cols, order_cols,
-                    minimum = False):
+                    minimum=False):
     df = df.dropna(axis=0, subset=order_cols)
     df = df.drop_duplicates()
     df.sort_values(order_cols, ascending=minimum, inplace=True)
@@ -31,10 +34,11 @@ def order_aggregate(df, id_cols,
     df = df.groupby(id_cols, as_index=False)[agg_cols]
     return df.agg(lambda x: x.iloc[0])
 
+
 def aggregate_data(df, uid, id_cols=[],
-                    mode_cols=[], max_cols=[],
-                    current_cols=[], time_col = "",
-                    make_uid=True):
+                   mode_cols=[], max_cols=[],
+                   current_cols=[], time_col="",
+                   make_uid=True):
     uid_col = [uid]
     agg_df = df[uid_col + id_cols].drop_duplicates()
     agg_df.reset_index(drop=True, inplace=True)
@@ -52,10 +56,10 @@ def aggregate_data(df, uid, id_cols=[],
                     order_aggregate(
                         df[uid_col + [time_col] + current_cols],
                         uid_col, current_cols, [time_col]),
-                    on = uid, how = 'left')
-        agg_df.rename(columns = dict(
+                    on=uid, how='left')
+        agg_df.rename(columns=dict(
                             zip(current_cols,
                                 ["Current." + tc for tc in current_cols])),
-                        inplace=True)
+                      inplace=True)
 
     return agg_df
