7148d12f4df7877c08aa0cacdb8f42128870e250
Roman Rivera <roman@invisibleinstitute.com>
2017-08-10 20:55:42 -0500

    resolving conflicts function + updated assign_unique_ids function

--- individual/share/src/assign_unique_ids_functions.py
+++ individual/share/src/assign_unique_ids_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def remove_duplicates(df, cols=[]):
@@ -11,11 +12,60 @@ def keep_duplicates(df, cols):
     return df[df.duplicated(subset=cols, keep=False)].sort_values(cols)
 
 
-def assign_unique_ids(df, uid, id_cols):
-    dfu = df[id_cols].drop_duplicates()
+def resolve_conflicts(df, id_cols, conflict_cols,
+                      uid, starting_uid,
+                      temp_fillna=-9999):
+    out_df = pd.DataFrame()
+
+    df.reset_index(drop=True, inplace=True)
+    df.fillna(temp_fillna, inplace=True)
+
+    group_df = df.groupby(id_cols, as_index=False)
+
+    for key, group in group_df:
+        group = group.reset_index(drop=True)
+        conflicts = 0
+
+        for col in conflict_cols:
+            non_nan = group[col]
+            non_nan = non_nan[(non_nan != -999) &
+                              (non_nan != temp_fillna)]
+            if len(non_nan.unique()) > 1:
+                conflicts += 1
+        if conflicts == 0:
+            group.insert(0, uid, starting_uid + 1)
+            starting_uid += 1
+        else:
+            group.insert(0, uid, starting_uid + group.index + 1)
+            starting_uid += max(group.index + 1)
+        out_df = out_df.append(group)
+
+    out_df[out_df == temp_fillna] = np.nan
+    return out_df
+
+
+def assign_unique_ids(df, uid, id_cols, conflict_cols=[]):
+    dfu = df[id_cols + conflict_cols].drop_duplicates()
     dfu.reset_index(drop=True, inplace=True)
-    dfu[uid] = dfu.index + 1
-    return df.merge(dfu, on=id_cols, how='left')
+    if conflict_cols:
+        rd_df = remove_duplicates(dfu, id_cols).reset_index(drop=True)
+        rd_df.insert(0, uid, rd_df.index + 1)
+
+        kd_df = keep_duplicates(dfu, id_cols).reset_index(drop=True)
+        rc_df = resolve_conflicts(kd_df, id_cols, conflict_cols,
+                                  uid=uid, starting_uid=max(rd_df[uid]))
+        rc_df = rd_df.append(rc_df)
+        df = df.merge(rc_df,
+                      on=id_cols + conflict_cols,
+                      how='left')
+
+        assert df[df[uid].isnull()].shape[0] != 0,\
+            print('Some unique IDs are null')
+        assert max(df[uid]) != len(df[uid].drop_duplicates()),\
+            print('Unique IDs are not correctly scaled')
+    else:
+        dfu[uid] = dfu.index + 1
+        return df.merge(dfu, on=id_cols, how='left')
 
 
 def max_aggregate(df, id_cols, max_cols):
@@ -37,8 +87,7 @@ def order_aggregate(df, id_cols,
 
 def aggregate_data(df, uid, id_cols=[],
                    mode_cols=[], max_cols=[],
-                   current_cols=[], time_col="",
-                   make_uid=True):
+                   current_cols=[], time_col=""):
     uid_col = [uid]
     agg_df = df[uid_col + id_cols].drop_duplicates()
     agg_df.reset_index(drop=True, inplace=True)
