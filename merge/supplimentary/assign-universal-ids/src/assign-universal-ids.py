1d4d18ad81ddef460833cd19271fac463bb48aa7
Roman Rivera <roman@invisibleinstitute.com>
2017-08-20 16:23:07 -0500

    adding supplementary merge files

---
+++ merge/supplimentary/assign-universal-ids/src/assign-universal-ids.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import __main__
+
+from assign_unique_ids_functions import aggregate_data
+from merge_functions import append_to_reference, listdiff, remerge
+import setup
+
+
+def get_setup():
+    ''' encapsulates args.
+        calls setup.do_setup() which returns constants and logger
+        constants contains args and a few often-useful bits in it
+        including constants.write_yamlvar()
+        logger is used to write logging messages
+    '''
+    script_path = __main__.__file__
+    args = {
+        'input_demo_files': [
+            'input/accused_demographics.csv.gz',
+            'input/witnesses_demographics.csv.gz',
+            'input/investigators_demographics.csv.gz',
+            'input/trr-officers_demographics.csv.gz',
+            'input/trr-statuses_demographics.csv.gz',
+            'input/awards_demographics.csv.gz'
+        ],
+        'input_full_files': [
+            'input/accused.csv.gz',
+            'input/witnesses.csv.gz',
+            'input/investigators.csv.gz',
+            'input/trr-officers.csv.gz',
+            'input/trr-statuses.csv.gz',
+            'input/awards.csv.gz'
+        ],
+
+        'output_files': [
+            'output/accused.csv.gz',
+            'output/witnesses.csv.gz',
+            'output/investigators.csv.gz',
+            'output/trr-officers.csv.gz',
+            'output/trr-statuses.csv.gz',
+            'output/awards.csv.gz'
+        ],
+        'output_profile_file': 'output/officer-profiles.csv.gz',
+        'output_reference_file': 'output/officer-reference.csv.gz',
+        'universal_id': 'UID'
+        }
+
+    assert all(input_demo_file.startswith('input/') and
+               input_demo_file.endswith('.csv.gz')
+               for input_demo_file in args['input_demo_files']),\
+        "An input_demo_file is malformed: {}".format(args['input_demo_files'])
+    assert all(input_full_file.startswith('input/') and
+               input_full_file.endswith('.csv.gz')
+               for input_full_file in args['input_full_files']),\
+        "An input_full_file is malformed: {}".format(args['input_full_files'])
+    assert all(output_file.startswith('output/') and
+               output_file.endswith('.csv.gz')
+               for output_file in args['output_files']),\
+        "An output_file is malformed: {}".format(args['output_files'])
+
+    return setup.do_setup(script_path, args)
+
+
+cons, log = get_setup()
+
+ref_df = pd.DataFrame()
+profile_df = pd.DataFrame()
+
+for idf, iff, of in zip(cons.input_demo_files,
+                        cons.input_full_files,
+                        cons.output_files):
+    sub_df = pd.read_csv(idf)
+    atr_dict = append_to_reference(sub_df=sub_df,
+                                   profile_df=profile_df,
+                                   ref_df=ref_df,
+                                   return_merge_report=True,
+                                   return_merge_list=True)
+
+    ref_df = atr_dict['ref']
+    cons.write_yamlvar('File added', idf)
+    cons.write_yamlvar('Officers Added',
+                       len(ref_df['UID']) - profile_df.shape[0])
+    if not profile_df.empty:
+        cons.write_yamlvar('Merge Report', atr_dict['MR'])
+        cons.write_yamlvar('Merge List',
+                           atr_dict['ML'].value_counts())
+
+    profile_df = aggregate_data(ref_df, cons.universal_id,
+                                mode_cols=listdiff(ref_df.columns,
+                                                   [cons.universal_id]))
+
+    full_df = pd.read_csv(iff)
+    id_col = [col for col in full_df.columns
+              if col.endswith('_ID')][0]
+    full_df = remerge(full_df, profile_df,
+                      cons.universal_id, id_col)
+    full_df.to_csv(of, **cons.csv_opts)
+
+profile_df.to_csv(cons.output_profile_file, **cons.csv_opts)
+ref_df.to_csv(cons.output_reference_file, **cons.csv_opts)
