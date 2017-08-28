4ac17509b5f222001ed58d7f598eab4950ce87a8
Roman Rivera <roman@invisibleinstitute.com>
2017-08-28 10:29:22 -0500

    slight changes/updates/clean ups

---
+++ individual/16-1105_all-sworn-employees/export/src/export.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import __main__
+
+import setup
+
+
+def get_setup():
+    ''' encapsulates args.
+        calls setup.do_setup() which returns constants and logger
+        constants contains args and a few often-useful bits in it
+        including constants.write_yamlvar()
+        logger is used to write logging messages
+    '''
+    script_path = __main__.__file__
+    args = {
+        'input_file': 'input/unit-history.csv.gz',
+        'input_demo_file': 'input/unit-history_demographics.csv.gz',
+        'output_file': 'output/unit-history.csv.gz',
+        'output_demo_file': 'output/unit-history_demographics.csv.gz',
+        'export_cols': ['Unit', 'Effective.Date', 'End.Date'],
+        'id': 'unit_history_ID'
+        }
+
+    assert (args['input_file'].startswith('input/') and
+            args['input_file'].endswith('.csv.gz')),\
+        "input_file is malformed: {}".format(args['input_file'])
+    assert (args['output_file'].startswith('output/') and
+            args['output_file'].endswith('.csv.gz')),\
+        "output_file is malformed: {}".format(args['output_file'])
+
+    return setup.do_setup(script_path, args)
+
+
+cons, log = get_setup()
+
+df = pd.read_csv(cons.input_file)
+df = df[[cons.id] + cons.export_cols]
+df.to_csv(cons.output_file, **cons.csv_opts)
+
+demo_df = pd.read_csv(cons.input_demo_file)
+demo_df.to_csv(cons.output_demo_file, **cons.csv_opts)
