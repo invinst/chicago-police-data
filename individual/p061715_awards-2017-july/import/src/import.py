c2ae2226058f510325955139ae665c57c7ff9572
Roman Rivera <roman@invisibleinstitute.com>
2017-08-31 12:47:42 -0500

    testing move

--- individual/p061715_awards-2017-july/import/src/import.py
+++ individual/p061715_awards-2017-july/import/src/import.py
@@ -14,21 +14,13 @@ def get_setup():
     '''
     script_path = __main__.__file__
     args = {
-        'input_files': [
-                        'input/Part_1.xlsx',
-                        'input/Part_2.xlsx',
-                        'input/Part_3.xlsx',
-                        'input/Part_4.xlsx',
-                        'input/Part_5.xlsx',
-                        'input/Part_6.xlsx'
-                       ],
+        'input_file': 'input/Awards_Data_(New_Copy).csv',
         'output_file': 'output/awards.csv.gz',
         'metadata_file': 'output/metadata_awards.csv.gz'
         }
 
-    assert all(input_file.startswith('input/')
-               for input_file in args['input_files']),\
-        "An input_file is malformed: {}".format(args['input_files'])
+    assert args['input_file'].startswith('input/'),\
+        "Input_file is malformed: {}".format(args['input_file'])
     assert (args['output_file'].startswith('output/') and
             args['output_file'].endswith('.csv.gz')),\
         "output_file is malformed: {}".format(args['output_file'])
@@ -38,35 +30,10 @@ def get_setup():
 
 cons, log = get_setup()
 
-data_df = pd.DataFrame()
-meta_df = pd.DataFrame()
 
-column_names = []
-total_rows = 0
+df = pd.read_csv(cons.input_file)
+df.columns = standardize_columns(df.columns)
+df.to_csv(cons.output_file, **cons.csv_opts)
 
-for input_file in cons.input_files:
-    if column_names:
-        df = pd.read_excel(input_file, header=None)
-    else:
-        df = pd.read_excel(input_file)
-        column_names = standardize_columns(df.columns)
-
-    total_rows += df.shape[0]
-
-    df.columns = column_names
-
-    data_df = (data_df
-               .append(df)
-               .reset_index(drop=True))
-
-    meta_df = (meta_df
-               .append(collect_metadata(df, input_file, cons.output_file))
-               .reset_index(drop=True))
-
-assert total_rows == data_df.shape[0],\
-        print(('Total Rows ({0}) must equal '
-               'appended rows ({1})').format(total_rows, data_df.shape[0]))
-
-data_df.to_csv(cons.output_file, **cons.csv_opts)
-
+meta_df = collect_metadata(df, cons.input_file, cons.output_file)
 meta_df.to_csv(cons.metadata_file, **cons.csv_opts)
