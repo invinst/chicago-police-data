4fc6ec1e12b431675528556d6dab21e6fc2feab6
Roman Rivera <roman@invisibleinstitute.com>
2017-10-13 13:07:36 -0500

    new officer complaints file

--- individual/p428703_officer-complaints-2017-sep/import/src/import.py
+++ individual/p428703_officer-complaints-2017-sep/import/src/import.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import __main__
 
-from clean_functions import clean_data
+from import_functions import collect_metadata
 import setup
 
 
@@ -14,12 +14,13 @@ def get_setup():
     '''
     script_path = __main__.__file__
     args = {
-        'input_file': 'input/officer-complaints.csv.gz',
-        'output_file': 'output/officer-complaints.csv.gz'
+        'input_file': 'input/tabula-FOIA_P428703_Responsive_Records.csv',
+        'output_file': 'output/officer-complaints.csv.gz',
+        'metadata_file': 'output/metadata_officer-complaints.csv.gz',
+        'column_names_key': 'p428703_officer-complaints-2017-sep'
         }
 
-    assert (args['input_file'].startswith('input/') and
-            args['input_file'].endswith('.csv.gz')),\
+    assert args['input_file'].startswith('input/'),\
         "input_file is malformed: {}".format(args['input_file'])
     assert (args['output_file'].startswith('output/') and
             args['output_file'].endswith('.csv.gz')),\
@@ -30,6 +31,10 @@ def get_setup():
 
 cons, log = get_setup()
 
-df = pd.read_csv(cons.input_file)
-df = clean_data(df)
+df = pd.read_csv(cons.input_file, header=None)
+df.columns = ['CR_ID']
+log.info('Only column labeled CR_ID')
 df.to_csv(cons.output_file, **cons.csv_opts)
+
+meta_df = collect_metadata(df, cons.input_file, cons.output_file)
+meta_df.to_csv(cons.metadata_file, **cons.csv_opts)
