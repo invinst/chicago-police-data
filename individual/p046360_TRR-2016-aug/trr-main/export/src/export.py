8800203082db5cb967cf201421b5e60b70e229e6
Roman Rivera <roman@invisibleinstitute.com>
2017-10-31 16:58:51 -0500

    added export src stuff for non-officer tasks

---
+++ individual/p046360_TRR-2016-aug/trr-main/export/src/export.py
@@ -0,0 +1,39 @@
+#!usr/bin/env python3
+#
+# Author(s):    Roman Rivera
+
+'''export script for p046360_TRR-2016-aug/trr-main'''
+
+import pandas as pd
+import __main__
+
+import setup
+
+
+def get_setup():
+    ''' encapsulates args.
+        calls setup.do_setup() which returns constants and logger
+        constants contains args and a few often-useful bits in it
+        including constants.write_yamlvar()
+        logger is used to write logging messages
+    '''
+    script_path = __main__.__file__
+    args = {
+        'input_file': 'input/trr-main.csv.gz',
+        'output_file': 'output/trr-main.csv.gz',
+        }
+
+    assert (args['input_file'].startswith('input/') and
+            args['input_file'].endswith('.csv.gz')),\
+        "input_file is malformed: {}".format(args['input_file'])
+    assert (args['output_file'].startswith('output/') and
+            args['output_file'].endswith('.csv.gz')),\
+        "output_file is malformed: {}".format(args['output_file'])
+
+    return setup.do_setup(script_path, args)
+
+
+cons, log = get_setup()
+
+df = pd.read_csv(cons.input_file)
+df.to_csv(cons.output_file, **cons.csv_opts)
