6a6f80e3cec5be6b2b6800a5027b541f3e4ce87c
Tony Hudson <thudson@civisanalytics.com>
2017-11-01 09:50:21 -0500

    class example

---
+++ examples/connections/dropbox_connection.py
@@ -0,0 +1,50 @@
+import dropbox
+import os
+import tempfile
+import io
+import sys
+import re
+
+class dropbox_handler:
+    def __init__(self):
+        self.auth_token = os.environ.get('DROPBOX_OAUTH_PASSWORD')
+        self.dbx = dropbox.Dropbox(self.auth_token)
+
+    def list_files(self, dbx_path):
+        res = self.dbx.files_list_folder(dbx_path)
+        return [entry.name for entry in res.entries]
+
+    def download_file(self,
+                      dbx_path,
+                      name,
+                      return_sheets=False,
+                      sheetname=None,
+                      skip=None,
+                      rows=None):
+        if dbx_path[-1]!='/':
+            dbx_path = dbx_path+'/'
+        with tempfile.NamedTemporaryFile(mode='w') as temp:
+            name = name.lower()
+            download_file = dbx_path + name
+            print(download_file)
+            self.dbx.files_download_to_file(temp.name,download_file)
+
+            temp.flush()
+            temp.seek(0)
+
+            if return_sheets==True:
+                return pd.ExcelFile(temp.name).sheet_names
+            elif '.csv' in name:
+                return pd.read_csv(temp.name,compression='gzip',low_memory=False)
+            elif '.xls' in name:
+                if sheetname==None:
+                    return pd.read_excel(temp.name,
+                                         skiprows=skip,
+                                         nrows=rows)
+                else:
+                    return pd.read_excel(temp.name,
+                                         sheet=sheetname,
+                                         skiprows=skip,
+                                         nrows=rows)
+            else:
+                print("Download of file not supported. File is not a .csv, .xls, or .xlsx")
---
+++ examples/import.py
@@ -0,0 +1,43 @@
+import civis
+import pandas as import pd
+from .connections.dropbox_connection import dropbox_handler
+
+client = civis.APIClient()
+dropbox = dropbox_handler()
+
+## paths where things are in dropbox
+path = '/Data/Roman/Output'
+folders = ['awards','complaints','profiles','ranks','rosters','TRR','unit-history']
+
+## Database info about the Invisible Institute cluster
+db = 'Invisible Institute'
+db_id = 791
+schema = 'output'
+## loops through folders and files, imports them to redshift and grants the group
+for folder in folders:
+    new_path = '/'.join([path,folder])
+    print(new_path)
+    upload_files = [file_name for file_name in dropbox.list_files(new_path) if '.csv.gz' in file_name]
+    for file_name in upload_files:
+        df = dropbox.download_file(new_path,file_name)
+        ## unit history tables share names with trrs so adding unit-history to the table name
+        if folder == 'unit-history':
+            file_name = '-'.join(['unit-history',file_name.split('.')[0]])
+            table_name = '.'.join([schema,file_name])
+        else:
+            table_name = '.'.join([schema,file_name.split('.')[0]])
+        print('Importing Table: {}'.format(table_name))
+        print('-----------------------------------')
+        try:
+            import_job = civis.io.dataframe_to_civis(df,
+                                                     database=db,
+                                                     table = table_name,
+                                                     existing_table_rows='fail')
+            ## waits for import to be done before starting next import
+            import_job.result()
+        except:
+            print('Import Job Failed: {}'.format(table_name))
+
+##grants the group on all tables in schema
+sql_job = 'grant all on all tables in schema {} to group invinst'.format(schema)
+client.queries.post(database=db_id,sql=sql_job,preview_rows=0)
