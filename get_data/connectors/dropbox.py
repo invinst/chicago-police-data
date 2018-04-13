e466a29bebd0ad9693e72b3f0d2f84fa0c7ffe76
Tony Hudson <thudson@civisanalytics.com>
2018-04-13 13:00:01 -0500

    dropbox connection;

---
+++ get_data/connectors/dropbox.py
@@ -0,0 +1,64 @@
+import dropbox
+import os
+import tempfile
+import pandas as pd
+import numpy as np
+import io
+import sys
+import re
+
+class dropbox_handler:
+    def __init__(self):
+        self.auth_token = os.environ.get('DROPBOX_OAUTH_PASSWORD')
+        self.dbx = dropbox.Dropbox(self.auth_token)
+
+    def list_files(self, dbx_path):
+        res = self.dbx.files_list_folder(dbx_path)
+        return [entry.name for entry in res.entries]
+
+    def download_directory(self,dbx_path):
+        res = self.dbx.files_list_folder(dbx_path,recursive=True)
+        filenames = [entry.path_lower for entry in res.entries]
+        for filename in filenames[1:]:
+            ## get name of last filepath
+            name = filename.split('/')[-1]
+            if '.' not in name[1:] and name!='makefile':
+                os.makedirs('/app/dropbox'+filename)
+            if '.' in name[1:] or name=='makefile':
+                download_file = '/app/dropbox'+filename
+                self.dbx.files_download_to_file(download_file,filename)
+
+    def download_file(self,
+                      dbx_path,
+                      name,
+                      return_sheets=False,
+                      sheetname=None,
+                      skip=None,
+                      rows=None):
+        if dbx_path[-1]!='/':
+            dbx_path = dbx_path+'/'
+        with tempfile.NamedTemporaryFile(mode='w') as temp:
+            name = name.lower()
+            download_file = dbx_path + name
+            print(download_file)
+            self.dbx.files_download_to_file(temp.name,download_file)
+
+            temp.flush()
+            temp.seek(0)
+
+            if return_sheets==True:
+                return pd.ExcelFile(temp.name).sheet_names
+            elif '.csv' in name:
+                return pd.read_csv(temp.name,compression='gzip',low_memory=False)
+            elif '.xls' in name:
+                if sheetname==None:
+                    return pd.read_excel(temp.name,
+                                         skiprows=skip,
+                                         nrows=rows)
+                else:
+                    return pd.read_excel(temp.name,
+                                         sheet=sheetname,
+                                         skiprows=skip,
+                                         nrows=rows)
+            else:
+                print("Download of file not supported. File is not a .csv, .xls, or .xlsx")
---
+++ get_data/run.py
@@ -0,0 +1,29 @@
+from .connectors.dropbox import dropbox_handler
+import civis
+import pandas as pd
+import numpy as np
+import os
+import argparse
+
+def init_args():
+    """Init"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_to_execute',
+                        default=os.environ.get('Dropbox_Path'))
+    return parser.parse_args()
+
+def run_file(path_to_execute):
+    full_path = 'dropbox'+path_to_execute
+    os.chdir(full_path)
+    print(os.listdir())
+    os.system("make -f src/makefile")
+
+if __name__=="__main__":
+    ARGUMENTS = init_args()
+    client = civis.APIClient()
+    dropbox = dropbox_handler()
+
+    ## paths where things are in dropbox
+    #print(dropbox.list_files(path))
+    dropbox.download_directory(ARGUMENTS.path_to_execute)
+    run_file(ARGUMENTS.path_to_execute)
