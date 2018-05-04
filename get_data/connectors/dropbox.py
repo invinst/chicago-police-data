12cd0c31fef7b5fe0e67a0097ab088502bba5ebd
Tony Hudson <thudson@civisanalytics.com>
2018-05-04 11:53:29 -0500

    print statements

--- get_data/connectors/dropbox.py
+++ get_data/connectors/dropbox.py
@@ -49,7 +49,7 @@ class dropbox_handler:
             with open(local_path+filename,'rb') as f:
                 self.dbx.files_upload(f.read(),
                                       dbx_output_path+filename,
-                                      mode=dbx.files.WriteMode('overwrite', None))
+                                      mode=self.dbx.files.WriteMode('overwrite', None))
 
     def download_file(self,
                       dbx_path,
