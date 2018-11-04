50bfaaff647039fd347face4657ee4cf73875254
Tony Hudson <thudson@civisanalytics.com>
2018-11-04 12:20:35 -0600

    lowercase folder names

--- get_data/move_in.py
+++ get_data/move_in.py
@@ -74,6 +74,8 @@ def append_to_folder_structure(folders, output_path_dict, file_type):
         frozen = output_path_dict['csv'] + output_path_dict['csv_file'].lower()
         input = folders + folder + '/import/input/' + \
             output_path_dict['csv_file'].lower()
+        print(frozen)
+        print(input)
         copy(frozen, input)
     return folder_structure
 
