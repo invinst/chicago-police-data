8c486ddb7aec46fa678660be90ee2e401b3387d1
Tony Hudson <thudson@civisanalytics.com>
2018-11-10 11:40:15 -0600

    remove /app calls

--- get_data/move_in.py
+++ get_data/move_in.py
@@ -16,15 +16,15 @@ def init_args():
                         default='TRR')
     # default=os.environ.get('file_type'))
     parser.add_argument('--data_parent_folder',
-                        default='/Data/Data_Testing_Copy/')
+                        default='Data/Data_Testing_Copy/')
     parser.add_argument('--pdf_location',
                         default='foia/')
     parser.add_argument('--csv_or_xlsx_location',
                         default='frozen/')
     parser.add_argument('--folders',
-                        default='/app/get_data/utils/folder_structures/')
+                        default='get_data/utils/folder_structures/')
     parser.add_argument('--individual',
-                        default='/Data/Data_Testing_Copy/individual/')
+                        default='Data/Data_Testing_Copy/individual/')
     parser.add_argument('--new_name',
                         default='test_run')
     # default=os.environ.get('folder_identifier'))
@@ -33,7 +33,7 @@ def init_args():
 
 def trr_handler(path_to_execute,
                 trr_filename):
-    trr_filename = '/app'+path_to_execute.lower() + '/' + trr_filename
+    trr_filename = path_to_execute.lower() + '/' + trr_filename
     output_trr_filename, trr_files = sterilize.sterilize(trr_filename)
     return output_trr_filename, trr_files
 
@@ -43,7 +43,7 @@ def create_path(data_parent_folder,
                 csv_or_xlsx_location,
                 path_to_execute):
 
-    downloaded_files = os.listdir('/app'+path_to_execute.lower())
+    downloaded_files = os.listdir(path_to_execute.lower())
     new_path = ('_').join(path_to_execute.split('/')[-2:]) + '/'
     output_path_dict = {}
     output_path_dict['pdf'] = (data_parent_folder + pdf_location + new_path)
@@ -53,19 +53,19 @@ def create_path(data_parent_folder,
     os.makedirs(output_path_dict['csv'])
 
     for file in downloaded_files:
-        file_path = '/app'+path_to_execute.lower() + '/' + file
+        file_path = path_to_execute.lower() + '/' + file
         if '.pdf' in file:
-            output_path = '/app' + output_path_dict['pdf']
+            output_path = output_path_dict['pdf']
             os.makedirs(output_path)
             output_path_dict['pdf_file'] = file
             copy(file_path, output_path + file)
         elif '.csv' in file or '.xlsx' in file:
-            output_path = '/app' + output_path_dict['csv']
+            output_path = output_path_dict['csv']
             os.makedirs(output_path)
             output_path_dict['csv_file'] = file
             copy(file_path, output_path + file)
             print('List Output Path Files: {}'.format(
-                os.listdir('/app' + output_path_dict['csv'])))
+                os.listdir(output_path_dict['csv'])))
             '''if 'trr' in file.lower():
                 output_trr_filename, trr_files = trr_handler(path_to_execute,
                                                              file)
@@ -86,7 +86,7 @@ def append_to_folder_structure(folders,
                                     folder_parameter_name,
                                     datetime.today().strftime('%Y%m%d')])
         new_folder_structure.append(new_folder_name)
-        frozen = '/app' + output_path_dict['csv'] + \
+        frozen = output_path_dict['csv'] + \
             output_path_dict['csv_file'].lower()
         input = folders + folder + '/import/input/' + \
             output_path_dict['csv_file'].lower()
