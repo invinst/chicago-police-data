21148f3c9b1a29ae202f277db647fe673e262579
Tony Hudson <thudson@civisanalytics.com>
2018-11-17 12:37:44 -0600

    replace print for logging

--- get_data/utils/sterilize.py
+++ get_data/utils/sterilize.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import numpy as np
 import xlsxwriter
-
+import logging
 
 def clean_cb(x):
     if isinstance(x, str):
@@ -26,16 +26,16 @@ def clean_cb(x):
 
 def process_data(filename):
     xl_wb = pd.ExcelFile(filename)
-    print("File loaded")
+    logging.info("File loaded")
 
     # Sterilize CB NOs
     main_df = xl_wb.parse("Sheet1")
     charges_df = xl_wb.parse("Charges")
-    print("Sheets loaded")
+    logging.info("Sheets loaded")
 
     main_df['SUBJECT_CB_NO'] = main_df['SUBJECT_CB_NO'].map(clean_cb)
     charges_df['SUBJECT_CB_NO'] = charges_df['SUBJECT_CB_NO'].map(clean_cb)
-    print("CBs cleaned")
+    logging.info("CBs cleaned")
 
     cb_df = pd.DataFrame(main_df['SUBJECT_CB_NO']
                          .append(charges_df['SUBJECT_CB_NO']),
@@ -44,7 +44,7 @@ def process_data(filename):
         .drop_duplicates() \
         .reset_index(drop=True)
     cb_df.insert(0, 'SUBJECT_NO', cb_df.index+1)
-    print("cb_df formed")
+    logging.info("cb_df formed")
 
     main_df = main_df.merge(cb_df, on="SUBJECT_CB_NO", how="left")
     charges_df = charges_df.merge(cb_df, on="SUBJECT_CB_NO", how="left")
@@ -57,7 +57,7 @@ def process_data(filename):
             .dropna(how='any')
             .drop_duplicates().shape[0]) == charges_df['SUBJECT_CB_NO'] \
         .nunique()
-    print("subject_no's added to dfs")
+    logging.info("subject_no's added to dfs")
 
     # Sterilize RD_NO
     rd_df = pd.DataFrame(main_df['RD_NO'].append(charges_df['RD_NO']),
@@ -67,7 +67,7 @@ def process_data(filename):
         .drop_duplicates()\
         .reset_index(drop=True)
     rd_df.insert(0, 'SR_NO', rd_df.index+1)
-    print("rd_df formed")
+    logging.info("rd_df formed")
 
     main_df = main_df.merge(rd_df, on="RD_NO", how="left")
     charges_df = charges_df.merge(rd_df, on="RD_NO", how="left")
@@ -87,7 +87,7 @@ def process_data(filename):
     assert not {'NONE'} & set(main_df['SR_NO'])
     assert not {'NONE'} & set(charges_df['SR_NO'])
 
-    print("SR_NO's added to dfs")
+    logging.info("SR_NO's added to dfs")
 
     # Sterilize EVENT_NO
     event_df = main_df[['EVENT_NO']]\
@@ -96,7 +96,7 @@ def process_data(filename):
         .drop_duplicates()\
         .reset_index(drop=True)
     event_df.insert(0, 'SE_NO', event_df.index+1)
-    print("event_df formed")
+    logging.info("event_df formed")
 
     main_df = main_df.merge(event_df, on="EVENT_NO", how="left")
 
@@ -107,7 +107,7 @@ def process_data(filename):
             .replace(0, np.nan)
             .nunique())
     assert not {0} & set(main_df['SE_NO'])
-    print("SE_NO's added to dfs")
+    logging.info("SE_NO's added to dfs")
     return xl_wb, main_df, charges_df
 
 
@@ -118,7 +118,7 @@ def write_files(output_filename,
                 xl_wb):
 
     writer = pd.ExcelWriter(output_filename, engine='xlsxwriter')
-    print("writer opened")
+    logging.info("writer opened")
 
     del main_df["SUBJECT_CB_NO"]
     del main_df["CR_NO_OBTAINED"]
@@ -126,22 +126,22 @@ def write_files(output_filename,
     del main_df["EVENT_NO"]
 
     main_df.to_excel(writer, 'Sheet1', index=False)
-    print("main_df written")
+    logging.info("main_df written")
 
     del charges_df["SUBJECT_CB_NO"]
     del charges_df["RD_NO"]
 
     charges_df.to_excel(writer, 'Charges', index=False)
-    print("charges_df written")
+    logging.info("charges_df written")
 
     for sn in xl_wb.sheet_names:
         if sn not in ["Sheet1", "Charges"]:
             xl_wb.parse(sn).to_excel(writer, sn, index=False)
-            print("{} sheet written".format(sn))
+            logging.info("{} sheet written".format(sn))
 
-    print("Saving...")
+    logging.info("Saving...")
     writer.save()
-    print("Done.")
+    logging.info("Done.")
     return xl_wb.sheet_names
 
 
