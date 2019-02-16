02aaaab4fd6f2457bcad312417dbf2760e713e35
Tony Hudson <thudson@civisanalytics.com>
2019-02-16 10:35:05 -0600

    run updates

--- get_data/run.py
+++ get_data/run.py
@@ -1,17 +1,78 @@
 from .connectors.dropbox import dropbox_handler
 import civis
-import pandas as pd
-import numpy as np
 import os
 import argparse
+import subprocess
+from shutil import copy
+import logging
+
+LOG = logging.getLogger()
 
 
 def init_args():
     """Init"""
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_to_execute',
-                        default=os.environ.get('Dropbox_Path'))
+                        default=os.environ.get('DROPBOX_PATH'))
     return parser.parse_args()
+
+
+def run_make(folder_path, submodule):
+    makefile_path = '/'.join([folder_path, submodule, 'src', 'Makefile'])
+    LOG.INFO('Executing Makefile: {}'.format(makefile_path))
+    subprocess.run(args=['make', '-f', makefile_path])
+
+
+def get_output_filename(path):
+    files = [x for x in os.listdir(path) if '.csv' in x]
+    if len(files) == 1:
+        return files[0]
+    else:
+        raise ValueError('Too many output files: {}'.format(files))
+
+
+def move_file_from_input_to_output(folder_path,
+                                   submodule,
+                                   structure):
+    input_path = '/'.join(folder_path, submodule, 'output')
+    filename = get_output_filename(input_path)
+    in_path = '/'.join([input_path, filename])
+    if submodule == 'import':
+        out_path = '/'.join(folder_path, 'clean', 'input', filename)
+    if submodule == 'clean':
+        if 'assign-unique-ids' in structure:
+            out_path = '/'.join(folder_path, 'assign-unique-ids',
+                                'input', filename)
+        else:
+            out_path = '/'.join(folder_path, 'export',
+                                'input', filename)
+    if submodule == 'assign-unique-ids':
+        out_path = '/'.join(folder_path, 'export', 'input', filename)
+    LOG.INFO('Moving file from: {}'.format(in_path))
+    LOG.INFO('Moving file to: {}'.format(out_path))
+    LOG.INFO('****************************')
+    copy(in_path, out_path)
+
+
+def execute_folder(folder_path):
+    structure = os.listdir()
+    if 'import' in structure:
+        run_make(folder_path, 'import')
+        move_file_from_input_to_output(folder_path,
+                                       'import',
+                                       structure)
+    if 'clean' in structure:
+        run_make(folder_path, 'clean')
+        move_file_from_input_to_output(folder_path,
+                                       'clean',
+                                       structure)
+    if 'assign-unique-ids' in structure:
+        run_make(folder_path, 'assign-unique-ids')
+        move_file_from_input_to_output(folder_path,
+                                       'assign-unique-ids',
+                                       structure)
+    if 'export' in structure:
+        run_make(folder_path, 'export')
 
 
 if __name__ == "__main__":
@@ -19,5 +80,10 @@ if __name__ == "__main__":
     client = civis.APIClient()
     dropbox = dropbox_handler()
 
-    # paths where things are in dropbox
+    # path where things are in dropbox
     dropbox.download_directory(ARGUMENTS.path_to_execute)
+
+    execute_folder(ARGUMENTS.path_to_execute)
+
+    # dropbox.upload_directory(ARGUMENTS.path_to_execute,
+    #                         local_dbx_same=True)
