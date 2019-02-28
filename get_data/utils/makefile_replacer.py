f92dd380044fcbf82650fe76abd61cd370ada2b3
Tony Hudson <thudson@civisanalytics.com>
2019-02-28 10:18:47 -0600

    gzip them csvs in makefiles

--- get_data/utils/makefile_replacer.py
+++ get_data/utils/makefile_replacer.py
@@ -59,10 +59,16 @@ def makefile_updater(input_file, output_file, Makefile):
     filenames_to_replace = filenames_to_replace + regex.findall(Makefile)
     for filename in filenames_to_replace:
         if 'input/' in filename:
-            input_file = 'input/' + input_file
+            if '.csv' in filename:
+                input_file = 'input/' + input_file + '.gz'
+            else:
+                input_file = 'input/' + input_file
             Makefile = Makefile.replace(filename, input_file)
         elif 'output/' in filename:
-            output_file = 'output/' + output_file
+            if '.csv' in filename:
+                output_file = 'output/' + output_file + '.gz'
+            else:
+                output_file = 'output/' + output_file
             Makefile = Makefile.replace(filename, output_file)
         else:
             logging.info("Neither input nor output: {}".format(filename))
